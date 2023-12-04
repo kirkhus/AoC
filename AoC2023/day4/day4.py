a13117279eb24f2cc276d180bc79ba1dad949096
kirkhus <lars@kirkhus.org>
2023-12-04 18:44:23 +0100

    Getting there...

--- AoC2023/day4/day4.py
+++ AoC2023/day4/day4.py
@@ -7,21 +7,29 @@ class Card:
     _winners = 0
 
     def __init__(self, line):
-        self._id = line.split(":")[0]
+        # Card   1:  4 16 87 61 11 37 43 25 49 17 | 54 36 14 55 83 58 43 15 87 17 97 11 62 75 37  4 49 80 42 61 20 79 25 24 16
+        self._id = int(re.findall(r'^Card\s+(\d+)', line)[0])
+        
         self._count = 1
         winner_numbers, your_numbers = re.split(r'\s*\|\s*', re.sub(r'^.*:\s*', '', line))
         winner_numbers = re.split("\s{1,2}", winner_numbers)
         your_numbers = re.split("\s{1,2}", your_numbers)
         self._winners = len(set(winner_numbers).intersection(your_numbers))
 
+    def get_id(self):  
+        return self._id
+
     def update_count(self, count):
         self._count = count
 
+    def get_count(self):
+        return self._count
+    
     def get_points(self):
         return self._count * self._winners
     
-    def __repr__(self):
-        return "Card: " + self._id + " " + str(self._count) + " " + str(self._winners)
+    def __str__(self):
+        return f"Card: {self._id} {self._count} {self._winners}"
 
 
 # fucntion for adding up the series
@@ -33,20 +41,32 @@ def series(n):
     else:
         return 2 * series(n - 1)
 
-cards = []
+cards = {}
 lines = [line.strip() for line in open("./input4.txt").readlines()]
 
 for line in lines:
     card = Card(line)
-    cards.append(card)
-
-
+    cards[card.get_id()] = card
 
 
 part1_sum = 0
-for card in cards:
+for card in cards.values() :
     part1_sum += series(card.get_points())
 
-print(part1_sum)
+for card in cards.values():
+    print(card)
+    if card.get_points() > 0:
+        for x in range(1, card.get_points() + 1):
+            id = card.get_id()
+            if id in cards:
+                cards[id].update_count(cards[id].get_count() + 1)
+                print("Added to card")
+            
 
+part2_sum = 0
+for card in cards.values() :
+    part2_sum += series(card.get_points())        
+
+print(part1_sum)
+print(part2_sum)
 
