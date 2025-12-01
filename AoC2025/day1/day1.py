8434320a531c0a7bdf9ccb057524395ca42069e6
kirkhus <lars@kirkhus.org>
2025-12-01 10:02:58 +0100

    Dag 1.

---
+++ AoC2025/day1/day1.py
@@ -0,0 +1,37 @@
+lines = open("day1.txt").read().splitlines()
+
+
+def part1():
+    dial = 50
+    count = 0
+    for l in lines:
+        direction, steps = l[0], int(l[1:])
+        if direction == "R":
+            dial = (dial + steps) % 100
+        else:
+            dial = (dial - steps) % 100
+        if dial == 0:
+            count += 1
+
+    return count
+
+
+def part2():
+    dial = 50
+    count = 0
+    for l in lines:
+        direction, steps = l[0], int(l[1:])
+        for _ in range(steps):
+            if direction == "R":
+                dial = (dial + 1) % 100
+            else:
+                dial = (dial - 1) % 100
+            if dial == 0:
+                count += 1
+
+    return count
+
+
+
+print ("Part 1:", part1())
+print ("Part 2:", part2())
