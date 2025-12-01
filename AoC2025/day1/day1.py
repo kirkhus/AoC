17fd921960e7811cfd4b60332513424ad96584d6
kirkhus <lars@kirkhus.org>
2025-12-01 10:15:48 +0100

    Refactoring.

--- AoC2025/day1/day1.py
+++ AoC2025/day1/day1.py
@@ -1,4 +1,16 @@
+import math
 lines = open("day1.txt").read().splitlines()
+
+
+
+def rotate_dial(dial, direction, steps):
+    if direction == "R":
+        dial = (dial + steps) % 100
+    else:
+        dial = (dial - steps) % 100
+    return dial
+
+
 
 
 def part1():
@@ -6,16 +18,11 @@ def part1():
     count = 0
     for l in lines:
         direction, steps = l[0], int(l[1:])
-        if direction == "R":
-            dial = (dial + steps) % 100
-        else:
-            dial = (dial - steps) % 100
+        dial = rotate_dial(dial, direction, steps)
         if dial == 0:
             count += 1
 
     return count
-
-
 def part2():
     dial = 50
     count = 0
@@ -28,7 +35,6 @@ def part2():
                 dial = (dial - 1) % 100
             if dial == 0:
                 count += 1
-
     return count
 
 
